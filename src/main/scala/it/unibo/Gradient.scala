9cd903eaa2e9ba1b27e0a11cd366d260bfde00d4
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-05-09 10:55:21 +0200

    chore: add movement as test

--- src/main/scala/it/unibo/Gradient.scala
+++ src/main/scala/it/unibo/Gradient.scala
@@ -5,6 +5,7 @@ import it.unibo.alchemist.model.interfaces.Position
 import it.unibo.distributedfrp.utils.Liftable.liftTwice
 import it.unibo.distributed.frp.lang.*
 import it.unibo.distributedfrp.core.Incarnation
+import it.unibo.distributedfrp.utils.Liftable.*
 import it.unibo.distributed.frp.lib.{GradientLib, IncarnationProvider}
 class Gradient extends ProgramFactory:
   def create[P <: Position[P]](incarnation: DistributedFrpIncarnation[P]): incarnation.Flow[?] =
