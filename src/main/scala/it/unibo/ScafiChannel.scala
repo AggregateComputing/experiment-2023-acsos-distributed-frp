a128dba41d464ee85599d3949fecb9c922b77ea4
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-05-13 10:50:51 +0200

    chore: add right extracto also for channel scafi

--- src/main/scala/it/unibo/ScafiChannel.scala
+++ src/main/scala/it/unibo/ScafiChannel.scala
@@ -5,5 +5,6 @@ import it.unibo.scafi.lib.*
 
 class ScafiChannel extends BaseScafiProgram with ScafiGradientLib with ScafiLeaderElectionLib:
   override def computation(): Any =
+    
     val result = branch(sense("obstacle"))(false)(channel(sense("source"), sense("destination"), 0.1))
     if result then 10 else 0.0
